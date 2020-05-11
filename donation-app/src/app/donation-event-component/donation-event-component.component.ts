cb3d8e525960dacb0338ddd9e6b532d7153150e7
Ashan Madushanka <ashan_m@3logixs.lk>
2020-05-11 23:35:07 +0530

    generate new component for donation event

---
+++ donation-app/src/app/donation-event-component/donation-event-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-event-component',
+  templateUrl: './donation-event-component.component.html',
+  styleUrls: ['./donation-event-component.component.scss']
+})
+export class DonationEventComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
